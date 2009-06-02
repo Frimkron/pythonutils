47d3ea4abc71ac165df2656b7abb6d6b2d1ac2a5
Mark <mfrimston@gmail.com>
2009-06-02 21:37:07 +0100

    Added test for search iteration limiting

--- src/mrf/tileutil.py
+++ src/mrf/tileutil.py
@@ -615,6 +615,18 @@ if __name__ == "__main__":
 							   (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9),
 							   (4, 9), (3, 9), (3, 8), (3, 7), (3, 6), (3, 5), (2, 5), (1, 5),
 							   (0, 5), (0, 4), (0, 3), (0, 2), (0, 1), (0, 0)], path)
+		
+		def testIsCompleted(self):
+			path = self.search.search((9,2),(0,0),2)
+			self.assertEquals(False, path)
+			self.assertEquals(True, self.search.search_in_progress())
+			while path==False:
+				path = self.search.resume(2)
+			self.assertEquals(False, self.search.search_in_progress())
+						
+			path = self.search.search((9,2),(9,1),2)
+			self.assertEquals(True, path!=False)
+			self.assertEquals(False, self.search.search_in_progress())
 
 	class TestTileRender(unittest.TestCase):
 		
