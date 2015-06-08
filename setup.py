34c1774a52584b227d58a90f044d8339a08a023e
mark <mfrimston@gmail.com>
2015-06-08 16:07:17 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='mrf',
+      version='1.0',
+      description='Miscellaneous Utilities',
+      author='Mark Frimston',
+      author_email='mfrimston@gmail.com',
+      url='http://github.com/Frimkron/pythonutils',
+      packages=['mrf'])
